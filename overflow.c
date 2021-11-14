5e5907c303104189d285a25567f47e3877d50ac2
Aidan <71911942+N0tA1dan@users.noreply.github.com>
2021-11-13 20:11:27 -0800

    Update overflow.c

--- overflow.c
+++ overflow.c
@@ -6,7 +6,7 @@ int main(void) {
     char bf2[10] = "Hello";
     char bf1[10] = "";
 
-    // strcpy just puts information into our char array.
+    // strcpy() just copies our information we give it into the char array
     // since bf1 can only hold 10 bytes of data and we put in 11. we get a buffer overflow and we overflow into some new memory.
     strcpy(bf1, "00000000000");
     printf(bf1);
